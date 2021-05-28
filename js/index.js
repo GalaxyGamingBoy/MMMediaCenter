3c85b06eeb63e2486b8622821887d255fdb07319
GalaxyGamingBoy <xrteach@gmail.com>
2021-05-28 16:21:49 +0300

    Added download link for 28.5.2021

--- js/index.js
+++ js/index.js
@@ -2,41 +2,55 @@
 var f20210517_button = document.getElementById("f20210517");
 var f20210517_button_r = document.getElementById("f20210517_r");
 var f20210521_button = document.getElementById("f20210521");
+var f20210528_button = document.getElementById("f20210528");
 var form_button = document.getElementById("form");
 function getCurrentTime() {
     var CurrentYear = new Date().getFullYear().toString();
     var CurrentMonth = new Date().getMonth().toString();
     var CurrentDay = new Date().getDate().toString();
-    var CurrentDate = CurrentDay.concat("-").concat(CurrentMonth.toString()).concat("-").concat(CurrentYear.toString());
+    var CurrentDate = CurrentDay.concat("-")
+        .concat(CurrentMonth.toString())
+        .concat("-")
+        .concat(CurrentYear.toString());
     console.log("Date: ".concat(CurrentDate.toString()));
     var CurrentHour = new Date().getHours().toString();
     var CurrentMinute = new Date().getMinutes().toString();
     var CurrentSecond = new Date().getSeconds().toString();
     var CurrentMillisecond = new Date().getMilliseconds().toString();
-    var CurrentTime = CurrentHour.concat(":").concat(CurrentMinute.toString()).concat(":").concat(CurrentSecond.toString()).concat(":").concat(CurrentMillisecond.toString());
+    var CurrentTime = CurrentHour.concat(":")
+        .concat(CurrentMinute.toString())
+        .concat(":")
+        .concat(CurrentSecond.toString())
+        .concat(":")
+        .concat(CurrentMillisecond.toString());
     console.log("Time: ".concat(CurrentTime.toString()));
     var UnixTimestamp = new Date().getTime().toString();
     console.log("Unix Timestampt: ".concat(UnixTimestamp.toString()));
 }
-f20210517_button.addEventListener('click', function () {
+f20210517_button.addEventListener("click", function () {
     console.log("Clicked On 2021.05.17");
     getCurrentTime();
     window.location.replace("https://www.dropbox.com/s/soqxdnrfc45mpnt/%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82%2017.mp3?dl=1");
 });
-f20210517_button_r.addEventListener('click', function () {
+f20210517_button_r.addEventListener("click", function () {
     console.log("Clicked On 2021.05.17_r");
     getCurrentTime();
     window.location.replace("https://www.dropbox.com/s/f6yo7thh56bbcwj/2021-5-17%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82%20RD.mp3?dl=1");
 });
-f20210521_button.addEventListener('click', function () {
+f20210521_button.addEventListener("click", function () {
     console.log("Clicked On 2021.05.21");
     getCurrentTime();
     window.location.replace("https://www.dropbox.com/s/e7cu3wprv5iylp4/2021-5-21%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82.mp3?dl=1");
 });
-form_button.addEventListener('click', function () {
+f20210528_button.addEventListener("click", function () {
+    console.log("Clicked On 2021.05.28");
+    getCurrentTime();
+    window.location.replace("https://raw.githubusercontent.com/GalaxyGamingBoy/MMMediaCenter/master/Sentences/2021-5-28%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82.mp3?token=AMUYX7MTUMM4K42KK6VB343AWDW5A");
+});
+form_button.addEventListener("click", function () {
     console.log("Form Button Clicked");
     getCurrentTime();
-    window.open("https://forms.gle/EZXx7JNGo5r7z63dA", '_blank');
+    window.open("https://forms.gle/EZXx7JNGo5r7z63dA", "_blank");
 });
 console.log("200");
 console.log("MMedia Center Finished Loading JS, TS, CSS, HTML Successfully");
--- js/index.ts
+++ js/index.ts
@@ -1,43 +1,73 @@
 // Variables IDS
-const f20210517_button = document.getElementById("f20210517") as HTMLButtonElement;
-const f20210517_button_r = document.getElementById("f20210517_r") as HTMLButtonElement;
-const f20210521_button = document.getElementById("f20210521") as HTMLButtonElement;
+const f20210517_button = document.getElementById(
+  "f20210517"
+) as HTMLButtonElement;
+const f20210517_button_r = document.getElementById(
+  "f20210517_r"
+) as HTMLButtonElement;
+const f20210521_button = document.getElementById(
+  "f20210521"
+) as HTMLButtonElement;
+const f20210528_button = document.getElementById(
+  "f20210528"
+) as HTMLButtonElement;
 const form_button = document.getElementById("form") as HTMLButtonElement;
 
-function getCurrentTime(){
-    var CurrentYear :String = new Date().getFullYear().toString();
-    var CurrentMonth :String = new Date().getMonth().toString();
-    var CurrentDay :String = new Date().getDate().toString();
-    var CurrentDate :String = CurrentDay.concat("-").concat(CurrentMonth.toString()).concat("-").concat(CurrentYear.toString());
-    console.log("Date: ".concat(CurrentDate.toString()));
-    var CurrentHour :String = new Date().getHours().toString();
-    var CurrentMinute :String = new Date().getMinutes().toString();
-    var CurrentSecond :String = new Date().getSeconds().toString();
-    var CurrentMillisecond :String = new Date().getMilliseconds().toString();
-    var CurrentTime :String = CurrentHour.concat(":").concat(CurrentMinute.toString()).concat(":").concat(CurrentSecond.toString()).concat(":").concat(CurrentMillisecond.toString());
-    console.log("Time: ".concat(CurrentTime.toString()));
-    var UnixTimestamp :String = new Date().getTime().toString();
-    console.log("Unix Timestampt: ".concat(UnixTimestamp.toString()))
+function getCurrentTime() {
+  var CurrentYear: String = new Date().getFullYear().toString();
+  var CurrentMonth: String = new Date().getMonth().toString();
+  var CurrentDay: String = new Date().getDate().toString();
+  var CurrentDate: String = CurrentDay.concat("-")
+    .concat(CurrentMonth.toString())
+    .concat("-")
+    .concat(CurrentYear.toString());
+  console.log("Date: ".concat(CurrentDate.toString()));
+  var CurrentHour: String = new Date().getHours().toString();
+  var CurrentMinute: String = new Date().getMinutes().toString();
+  var CurrentSecond: String = new Date().getSeconds().toString();
+  var CurrentMillisecond: String = new Date().getMilliseconds().toString();
+  var CurrentTime: String = CurrentHour.concat(":")
+    .concat(CurrentMinute.toString())
+    .concat(":")
+    .concat(CurrentSecond.toString())
+    .concat(":")
+    .concat(CurrentMillisecond.toString());
+  console.log("Time: ".concat(CurrentTime.toString()));
+  var UnixTimestamp: String = new Date().getTime().toString();
+  console.log("Unix Timestampt: ".concat(UnixTimestamp.toString()));
 }
-f20210517_button.addEventListener('click', ()=>{
-    console.log("Clicked On 2021.05.17");
-    getCurrentTime();
-    window.location.replace("https://www.dropbox.com/s/soqxdnrfc45mpnt/%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82%2017.mp3?dl=1");
-})
-f20210517_button_r.addEventListener('click', ()=>{
-    console.log("Clicked On 2021.05.17_r");
-    getCurrentTime();
-    window.location.replace("https://www.dropbox.com/s/f6yo7thh56bbcwj/2021-5-17%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82%20RD.mp3?dl=1");
-})
-f20210521_button.addEventListener('click', ()=>{
-    console.log("Clicked On 2021.05.21");
-    getCurrentTime();
-    window.location.replace("https://www.dropbox.com/s/e7cu3wprv5iylp4/2021-5-21%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82.mp3?dl=1");
-})
-form_button.addEventListener('click', ()=>{
-    console.log("Form Button Clicked");
-    getCurrentTime();
-    window.open("https://forms.gle/EZXx7JNGo5r7z63dA", '_blank');
-})
+f20210517_button.addEventListener("click", () => {
+  console.log("Clicked On 2021.05.17");
+  getCurrentTime();
+  window.location.replace(
+    "https://www.dropbox.com/s/soqxdnrfc45mpnt/%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82%2017.mp3?dl=1"
+  );
+});
+f20210517_button_r.addEventListener("click", () => {
+  console.log("Clicked On 2021.05.17_r");
+  getCurrentTime();
+  window.location.replace(
+    "https://www.dropbox.com/s/f6yo7thh56bbcwj/2021-5-17%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82%20RD.mp3?dl=1"
+  );
+});
+f20210521_button.addEventListener("click", () => {
+  console.log("Clicked On 2021.05.21");
+  getCurrentTime();
+  window.location.replace(
+    "https://www.dropbox.com/s/e7cu3wprv5iylp4/2021-5-21%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82.mp3?dl=1"
+  );
+});
+f20210528_button.addEventListener("click", () => {
+  console.log("Clicked On 2021.05.28");
+  getCurrentTime();
+  window.location.replace(
+    "https://raw.githubusercontent.com/GalaxyGamingBoy/MMMediaCenter/master/Sentences/2021-5-28%20%CE%A0%CF%81%CE%BF%CF%84%CE%AC%CF%83%CE%B5%CE%B9%CF%82.mp3?token=AMUYX7MTUMM4K42KK6VB343AWDW5A"
+  );
+});
+form_button.addEventListener("click", () => {
+  console.log("Form Button Clicked");
+  getCurrentTime();
+  window.open("https://forms.gle/EZXx7JNGo5r7z63dA", "_blank");
+});
 console.log("200");
 console.log("MMedia Center Finished Loading JS, TS, CSS, HTML Successfully");
